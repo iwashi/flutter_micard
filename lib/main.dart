d916a64516bed794787f76788253ebfe457cd2c3
iwashi <iwase.yoshimasa@gmail.com>
2021-04-25 23:21:49 +0900

    Sec 45

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,35 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
-            color: Colors.white,
-            child: Text('hello'),
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/my.jpg'),
+              ),
+              Text(
+                'Yoshimasa Iwase',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'Enginner',
+                style: TextStyle(
+                  color: Colors.teal.shade100,
+                  fontSize: 10.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
 }
-
 
